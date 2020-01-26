657405c43c0f65c2eeea3d7b9a1a9fda9a00cadd
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-01-26 09:21:47 -0500

    Added Calc buttons (no func.)

--- src/apps/Calculator/App.vue
+++ src/apps/Calculator/App.vue
@@ -1,6 +1,42 @@
 <template>
   <div>
-    Calculator
+    <v-container fluid class="pb-0">
+      <v-row>
+        <v-col sm="12">
+          <span v-for="(item, index) in history" :key="index">{{ item }}</span><br>
+          <span class="display-1">{{ history[history.length - 1] }}</span>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col v-ripple @click="history = []" sm="3">AC</v-col>
+        <v-col v-ripple @click="history.push('+/-')" sm="3">+/-</v-col>
+        <v-col v-ripple @click="history.push('%')" sm="3">%</v-col>
+        <v-col v-ripple @click="history.push('+')" sm="3">+</v-col>
+      </v-row>
+      <v-row>
+        <v-col v-ripple @click="history.push(7)" sm="3">7</v-col>
+        <v-col v-ripple @click="history.push(8)" sm="3">8</v-col>
+        <v-col v-ripple @click="history.push(9)" sm="3">9</v-col>
+        <v-col v-ripple @click="history.push('-')" sm="3">-</v-col>
+      </v-row>
+      <v-row>
+        <v-col v-ripple @click="history.push(4)" sm="3">4</v-col>
+        <v-col v-ripple @click="history.push(5)" sm="3">5</v-col>
+        <v-col v-ripple @click="history.push(6)" sm="3">6</v-col>
+        <v-col v-ripple @click="history.push('*')" sm="3">*</v-col>
+      </v-row>
+      <v-row>
+        <v-col v-ripple @click="history.push(1)" sm="3">1</v-col>
+        <v-col v-ripple @click="history.push(2)" sm="3">2</v-col>
+        <v-col v-ripple @click="history.push(3)" sm="3">3</v-col>
+        <v-col v-ripple @click="history.push('/')" sm="3">/</v-col>
+      </v-row>
+      <v-row>
+        <v-col v-ripple @click="history.push(0)" sm="6">0</v-col>
+        <v-col v-ripple @click="history.push('.')" sm="3">.</v-col>
+        <v-col v-ripple @click="history.push('=')" sm="3">=</v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -9,7 +45,7 @@ export default {
   name: 'Calculator',
   data() {
     return {
-
+      history: []
     }
   }
 }
